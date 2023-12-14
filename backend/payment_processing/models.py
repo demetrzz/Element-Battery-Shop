86da4c02df618209e8fdf28a84c2fac49786fc0c
demetrzz <paha.gtn@gmail.com>
2023-12-14 18:24:53 +0300

    adding payment, shopping models

--- backend/payment_processing/models.py
+++ backend/payment_processing/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from shopping.models import Order
+
+
+class Payment(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='Заказ')
+    PENDING = 'pending'
+    WAITING_FOR_CAPTURE = 'waiting_for_capture'
+    SUCCEEDED = 'succeeded'
+    CANCELED = 'canceled'
+    STATUS_CHOICES = [
+        (PENDING, 'Ожидает оплаты'),
+        (WAITING_FOR_CAPTURE, 'Оплачен, ожидает подтверждения'),
+        (SUCCEEDED, 'Оплачен и подтвержден'),
+        (CANCELED, 'Отменен')
+    ]
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, db_index=True, verbose_name='Статус платежа')
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма платежа')
--- backend/shopping/models.py
+++ backend/shopping/models.py
@@ -1,26 +1,56 @@
 from django.db import models
 
 
-class Item(models.Model):
-    name = models.CharField(max_length=200, verbose_name='Наименование')
-    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Цена')
-    description = models.TextField(blank=True, verbose_name='Описание')
-    image = models.FileField(upload_to='items/', null=True, blank=True, verbose_name='Изображение')
-    quantity_in_stock = models.PositiveIntegerField(default=0, blank=True, verbose_name='Количество на складе')
+class Category(models.Model):
+    parent = models.ForeignKey('self', related_name='children', on_delete=models.CASCADE, blank=True, null=True)
+    name = models.CharField(max_length=200, null=True, verbose_name='Название категории')
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, verbose_name='Категория')
+    name = models.CharField(max_length=200, verbose_name='Наименование товара')
+    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Цена единицы товара')
+    description = models.TextField(blank=True, verbose_name='Описание товара')
+    image = models.FileField(upload_to='items/', null=True, blank=True, verbose_name='Изображение товара')
+    quantity_in_stock = models.PositiveIntegerField(default=0, blank=True, verbose_name='Количество товара на складе')
+
+    def __str__(self):
+        return self.name
+
+
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=200, null=True, verbose_name='Описание статуса заказа')
 
     def __str__(self):
         return self.name
 
 
 class Order(models.Model):
-    items = models.ManyToManyField(Item, through='OrderItem')
-    customer_name = models.CharField(max_length=200, verbose_name='Имя покупателя', blank=True)
+    products = models.ManyToManyField(Product, through='OrderItem')
     email = models.EmailField(verbose_name='Почта покупателя', blank=True)
-    phone = models.CharField(max_length=50, verbose_name='Телефон покупателя', blank=True)
     items_cost = models.IntegerField(default=0, verbose_name='Стоимость товаров')
+
+    CREATED = 'CREATED'
+    WAITING_PAYMENT = 'WAITING_PAYMENT'
+    PAYED = 'PAYED'
+    DELIVERY = 'DELIVERY'
+    COMPLETED = 'COMPLETED'
+    CANCELED = 'CANCELED'
+    STATUS_CHOICES = [
+        (CREATED, 'Создан'),
+        (WAITING_PAYMENT, 'Ожидает оплаты'),
+        (PAYED, 'Оплачен'),
+        (DELIVERY, 'В процессе доставки'),
+        (COMPLETED, 'Выполнен'),
+        (CANCELED, 'Отменен')
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=CREATED, verbose_name='Статус')
 
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='Заказ', related_name='items')
-    item = models.ForeignKey(Item, on_delete=models.CASCADE, verbose_name='Товар')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Товар')
     quantity = models.PositiveIntegerField(default=1, verbose_name='Количество')
